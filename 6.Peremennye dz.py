b9f53d6d01bf949df0337e7585a29e362ed27de2
an94york <an.chernuxa@yandex.ru>
2024-08-14 23:32:58 +0300

    Add files via upload

---
+++ 6.Peremennye dz.py
@@ -0,0 +1,13 @@
+number_of_homework_assignments_completed = (12)
+number_of_hours_spent = (1.5)
+course_name = "Python"
+time_for_one_task = (number_of_hours_spent / number_of_homework_assignments_completed) # при делении переменных вместо 2-х // нужно использовать 1 / ?
+print(number_of_homework_assignments_completed, number_of_hours_spent, course_name, time_for_one_task)
+print("Курс:", course_name,",","всего задач:", number_of_homework_assignments_completed,",", "затрачено часов:",
+      number_of_hours_spent,",", "среднее время выполнения:", time_for_one_task, "часов") # не понимаю как написать, чтобы запятые были сразу после переменных, а не через пробел
+
+number_of_homework = (12)
+hours = (1.5)
+course_name = "Python"
+time_for_one_task1 = (hours / number_of_homework)
+print("Курс:",course_name,", всего задач:", number_of_homework,", затрачено часов:", hours,", среднее время выполнения:", time_for_one_task1, "часов.")
